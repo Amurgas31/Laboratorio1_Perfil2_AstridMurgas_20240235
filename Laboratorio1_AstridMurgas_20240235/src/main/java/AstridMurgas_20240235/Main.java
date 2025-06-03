96c57cff9bfbacec062386fe2e276bee24da639c
Astrid <20240235@ricaldone.edu.sv>
2025-06-03 09:32:05 -0600

    Primer cambio

---
+++ Laboratorio1_AstridMurgas_20240235/src/main/java/AstridMurgas_20240235/Main.java
@@ -0,0 +1,9 @@
+package AstridMurgas_20240235;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> productos = new HashSet<>();
+    }
+}
